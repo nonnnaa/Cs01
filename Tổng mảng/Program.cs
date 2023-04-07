d26db672e82936e2041f5c0266c9ed2914b8a8ad
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-07 14:31:15 +0700

    Cs CG

---
+++ Tổng mảng/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+            int ans = 0;
+            foreach(var x in numbers)
+            {
+                ans += x;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ Tự học c#/List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            List<int> l = new List<int>();
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i=0 ; i<n ; i++)
+            {
+                l[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach(int x in l)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
