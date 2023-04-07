c576ced10f6f492e31f35f46c1faf2228360fdcd
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-07 15:28:33 +0700

    test

---
+++ Tìm giá trị trong mảng/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            string[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+            String input_name = Console.ReadLine();
+            bool check = false;
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Equals(input_name))
+                {
+                    Console.WriteLine("Position of the students in the list " + input_name + " is: " + (i + 1));
+                    check = true;
+                    break;
+                }
+            }
+            if(!check) Console.WriteLine("Not found" + input_name + " in the list.");
+        }
+    }
+}
